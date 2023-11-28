81c3110c178a56157795b4bde314da1b8d0c3f4f
WTNB1225 <w.swallowsfan@gmail.com>
2023-11-28 15:27:28 +0900

    works[id] fixed

--- front/app/works/[id]/page.tsx
+++ front/app/works/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "@/styles/page.module.scss";
 
@@ -14,9 +14,7 @@ export type Data = {
   updated_at: string;
 };
 
-export async function getImagesData() {
-  const pathname = usePathname();
-  const path_id = pathname.split("/")[2];
+export async function getImagesData(path_id:string) {
   const res = await fetch(`http://localhost:4000/api/v1/posts/${path_id}`);
   const data = await res.json();
   const dataurl = data.images_url;
@@ -25,17 +23,31 @@ export async function getImagesData() {
   return { dataurl, title, content };
 }
 
-export default async function WorkPages() {
-  const datas = await getImagesData();
-  const data = datas.dataurl;
-  const title = datas.title;
-  const content = datas.content;
+export default function WorkPages() {
+  const pathname = usePathname();
+  const path_id = pathname.split("/")[2];
+  const [url, setUrl] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  useEffect(() => {
+    getImagesData(path_id)
+      .then((p) => {
+        console.log(p);
+        setUrl(p.dataurl);
+        setTitle(p.title);
+        setContent(p.content);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  }, []);
+
   return (
     <>
       <div className={styles.margin}>
         <h1>{title}</h1>
         <p className={`${styles.ptag} ${styles.whitespace}`}>{content}</p>
-        {data.map((post: any) => {
+        {url.map((post: any) => {
           return (
             <Image key={post} src={post} width={400} height={300} alt="" />
           );
