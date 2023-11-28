500ab6346e141360c8d25009da33bed6258b55de
WTNB1225 <w.swallowsfan@gmail.com>
2023-11-28 21:14:17 +0900

    changed primarykey

--- back/db/migrate/20231120012256_create_users.rb
+++ back/db/migrate/20231120012256_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
-    create_table :users do |t|
+    create_table :users, id:false do |t|
       t.string :name, primary_key: true
       t.string :password_digest
 
